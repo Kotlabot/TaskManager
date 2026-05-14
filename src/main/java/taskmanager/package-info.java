cc32fbe9c2e6e866a16d1ca88b0542c87d116a1b
Tereza Kotlabova <terezka.kotlabova@gmail.com>
2026-05-14 11:35:50 +0200

    Add overview information about Task Manager application

--- src/main/java/taskmanager/package-info.java
+++ src/main/java/taskmanager/package-info.java
@@ -1,5 +1,20 @@
 /**
- * Console task manager application.
- * Provides task management, sorting and saving/loading.
+ * Task Manager application.
+ *
+ * <p>This package contains a console-based task management system that allows users to:
+ * <ul>
+ *   <li>Create, edit, delete and search tasks</li>
+ *   <li>Sort tasks by priority and deadline</li>
+ *   <li>Store and load tasks from TXT and JSON files</li>
+ *   <li>View task statistics (completion rate, deadlines, types, average priority)</li>
+ * </ul>
+ *
+ * <p>The application is designed as a simple personal tool and demonstrates:
+ * <ul>
+ *   <li>Object-oriented programming principles</li>
+ *   <li>File saving and loading operations</li>
+ *   <li>Collections and sorting algorithms</li>
+ *   <li>JSON (de)serialization using Jackson</li>
+ * </ul>
  */
 package taskmanager;
